0d0683d43c6fb8abd78a504271d0e0761867480b
WangB <wxjwb@qq.com>
2018-04-08 16:14:09 +0800

    update login api

--- src/main/java/com/caipiao/server/member/controller/MemberController.java
+++ src/main/java/com/caipiao/server/member/controller/MemberController.java
@@ -1,4 +1,5 @@
 package com.caipiao.server.member.controller;
+
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,16 +46,20 @@ public class MemberController<T> {
 			String token = param.getToken();
 			String account = param.getAccount();
 			MemberInfoResponse memberInfo = new MemberInfoResponse();
-			Member member = RedisUtils.getSession(token,MemberConstant.MEMBER);
-			if(member == null || account==null || !account.equals(member.getAccount())) {				
-				responset = new ResponseData<MemberInfoResponse>(MemberResponseStatus.LOGIN_INFO_ERROR, memberInfo);
-			}else {
-				memberInfo.setAccount(account);
-				memberInfo.setMobile(member.getMobile());
-				memberInfo.setNickname(member.getNickname());
-				memberInfo.setEmail(member.getEmail());
-				memberInfo.setAddress("ss");
-				responset = new ResponseData<MemberInfoResponse>(CommonResponseStatus.SUCCESS, memberInfo);
+			if (token == null || account == null) {
+				responset = new ResponseData<MemberInfoResponse>(MemberResponseStatus.NOT_LOGIN, memberInfo);
+			} else {
+				Member member = RedisUtils.getSession(token, MemberConstant.MEMBER);
+				if (member == null || account == null || !account.equals(member.getAccount())) {
+					responset = new ResponseData<MemberInfoResponse>(MemberResponseStatus.LOGIN_INFO_ERROR, memberInfo);
+				} else {
+					memberInfo.setAccount(account);
+					memberInfo.setMobile(member.getMobile());
+					memberInfo.setNickname(member.getNickname());
+					memberInfo.setEmail(member.getEmail());
+					memberInfo.setAddress("ss");
+					responset = new ResponseData<MemberInfoResponse>(CommonResponseStatus.SUCCESS, memberInfo);
+				}
 			}
 		} catch (Exception e) {
 			responset = new ResponseData<MemberInfoResponse>(CommonResponseStatus.EXCEPTION);
@@ -67,6 +72,7 @@ public class MemberController<T> {
 
 	/**
 	 * 登录接口{login}
+	 * 
 	 * @param param
 	 * @return
 	 */
@@ -76,9 +82,9 @@ public class MemberController<T> {
 		Member member = memberService.getMember(param.getAccount(), param.getPassword());
 		if (member == null) {
 			responset = new ResponseData<DataBody>(MemberResponseStatus.LOGIN_ERROR, null);
-		} else if(member.getStatus() == 0){
+		} else if (member.getStatus() == 0) {
 			responset = new ResponseData<DataBody>(MemberResponseStatus.MEMBER_LOCK_ERROR, null);
-		}else {
+		} else {
 			LoginReponse responsetBody = new LoginReponse();
 			responsetBody.setAccount(member.getAccount());
 			responsetBody.setToken(createToken(member));
@@ -87,15 +93,16 @@ public class MemberController<T> {
 		}
 		return getResponseContent(responset);
 	}
-	
+
 	public String createToken(Member member) {
-		String msj = member.toString()+System.currentTimeMillis();
+		String msj = member.toString() + System.currentTimeMillis();
 		String token = MD5.sign(msj);
 		return token;
 	}
 
-	public  ResponseEntity<ResponseData<DataBody>> getResponseContent(ResponseData<DataBody> responseData) {
-		ResponseEntity<ResponseData<DataBody>> response = new ResponseEntity<ResponseData<DataBody>>(responseData, HttpStatus.OK);
+	public ResponseEntity<ResponseData<DataBody>> getResponseContent(ResponseData<DataBody> responseData) {
+		ResponseEntity<ResponseData<DataBody>> response = new ResponseEntity<ResponseData<DataBody>>(responseData,
+				HttpStatus.OK);
 		return response;
 	}
 
@@ -109,7 +116,7 @@ public class MemberController<T> {
 			member.setEmail(regist.getEmail());
 			member.setMobile(regist.getMobile());
 			member.setNickname(regist.getNikename());
-			member.setPassword(regist.getPassword());
+			member.setPassword(MD5.sign(regist.getPassword()));
 			member.setStatus(1);
 			memberService.regist(member);
 			RegistReponse responsetBody = new RegistReponse();
@@ -131,18 +138,19 @@ public class MemberController<T> {
 		System.out.println(response.getBody());
 	}
 
-	/*public ResponseEntity<SmsSendResponse> doSend(String mobile, String code) {
-		final String sendUrl = "http://message-service/message/sms/send";
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-		map.add("mobile", mobile);
-		map.add("templateId", "CHECK_CODE");
-		map.add("params['code']", code);
-		log.info("发送参数：{}", map);
-
-		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-		ResponseEntity<SmsSendResponse> response = restTemplate.postForEntity(sendUrl, request, SmsSendResponse.class);
-		return response;
-	}*/
+	/*
+	 * public ResponseEntity<SmsSendResponse> doSend(String mobile, String code) {
+	 * final String sendUrl = "http://message-service/message/sms/send"; HttpHeaders
+	 * headers = new HttpHeaders();
+	 * headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+	 * MultiValueMap<String, String> map = new LinkedMultiValueMap<String,
+	 * String>(); map.add("mobile", mobile); map.add("templateId", "CHECK_CODE");
+	 * map.add("params['code']", code); log.info("发送参数：{}", map);
+	 * 
+	 * HttpEntity<MultiValueMap<String, String>> request = new
+	 * HttpEntity<MultiValueMap<String, String>>(map, headers);
+	 * ResponseEntity<SmsSendResponse> response =
+	 * restTemplate.postForEntity(sendUrl, request, SmsSendResponse.class); return
+	 * response; }
+	 */
 }
